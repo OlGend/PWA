e9d6962cc2a0d0763f5047e4abbef68a7d483ecf
Oleh <gend4016@gmail.com>
2024-01-24 18:41:30 +0200

    add post

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-	reactStrictMode: true,
+  reactStrictMode: true,
 };
 const withPWA = require("next-pwa");
 module.exports = withPWA({
-	pwa: {
-		dest: "public",
-		register: true,
-        disable: process.env.NODE_ENV ===      'development',
-		skipWaiting: true,
-	},
+  pwa: {
+    dest: "public",
+    register: true,
+    disable: process.env.NODE_ENV === "development",
+    skipWaiting: true,
+  },
 });
