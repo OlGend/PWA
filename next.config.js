7065e051fabade5f30decc125b9522012f3a4c73
Oleh <gend4016@gmail.com>
2024-01-24 16:31:49 +0200

    add post

--- next.config.js
+++ next.config.js
@@ -12,6 +12,20 @@ const {
     if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
       const withPWA = require("@ducanh2912/next-pwa").default({
         dest: "public",
+        fallbacks: {
+          // Failed page requests fallback to this.
+          document: "/~offline",
+          // This is for /_next/.../.json files.
+          data: "/fallback.json",
+          // This is for images.
+          image: "/fallback.webp",
+          // This is for audio files.
+          audio: "/fallback.mp3",
+          // This is for video files.
+          video: "/fallback.mp4",
+          // This is for fonts.
+          font: "/fallback-font.woff2",
+        },
       });
       return withPWA(nextConfig);
     }
