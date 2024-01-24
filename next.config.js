e0f2b3938dcbcc3cff5df5d2d97ebbc5d8fdb960
Oleh <gend4016@gmail.com>
2024-01-24 19:28:19 +0200

    add post

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
-const withPWA = require("next-pwa");
+const {
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_PRODUCTION_BUILD,
+  } = require("next/constants");
+  
+  /** @type {import("next").NextConfig} */
+  const nextConfig = {
+    reactStrictMode: true,
+  };
+  
+  module.exports = (phase) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+      const withPWA = require("@ducanh2912/next-pwa").default({
+        dest: "public",
+      });
+      return withPWA(nextConfig);
+    }
+    return nextConfig;
+  };
 
-module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    disable: process.env.NODE_ENV === "development",
-    skipWaiting: true,
-  },
-});
+
+
+ 
+  
