dba658e110bab4d987500c7a4af541a1a972b526
Oleh <gend4016@gmail.com>
2024-02-29 12:54:24 +0200

    pwa

---
+++ app/offline/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+
+const Offline = () => {
+  return (
+    <div>
+      <div className="unsubscribed">
+        <div className="container">
+          `<h2 className="text-center">You are Offline</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Offline;
